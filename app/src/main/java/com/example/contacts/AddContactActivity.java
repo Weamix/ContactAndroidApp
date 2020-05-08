6822e2cf1c8b3007b6b80c1323092405a0ecb8a0
Weamix <maxime.vitse@odaxiom.com>
2020-05-08 20:48:06 +0200

    starting add the contact to the db and display it on the main activity

--- app/src/main/java/com/example/contacts/AddContactActivity.java
+++ app/src/main/java/com/example/contacts/AddContactActivity.java
@@ -1,15 +1,66 @@
 package com.example.contacts;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class AddContactActivity extends AppCompatActivity {
+    private ContactsDbAdapter db;
+    public static EditText name;
+    public static EditText firstname;
+    public static EditText phone;
+    public static EditText email;
+    public static EditText address;
+    public static Button add_contact;
+    public static ListView list;
+
+    private void fillData() {
+        // Get all of the contacts from the database and create the item list
+        Cursor c = db.fetchAllContacts();
+        startManagingCursor(c);
+
+        String[] from = new String[] { ContactsDbAdapter.KEY_NAME };
+        int[] to = new int[] { R.id.name,R.id.firstname };
+
+        // Now create an array adapter and set it to display using our row
+        SimpleCursorAdapter notes =
+                new SimpleCursorAdapter(this, R.layout.activity_list_contacts, c, from, to);
+        list.setAdapter(notes);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
 
-        return;
+
+        db = new ContactsDbAdapter(this);
+        db.open();
+        fillData();
+
+        add_contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //listContacts.add(0, name.getText().toString(),firstname.getText().toString());
+                db.createContact(name.getText().toString(),firstname.getText().toString(),phone.getText().toString(),email.getText().toString(),address.getText().toString());
+                name.setText("");
+                firstname.setText("");
+                phone.setText("");
+                email.setText("");
+                address.setText("");
+                fillData();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/contacts/MainActivity.java
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
     public static Button button;
+    private static ArrayList<String> listContacts;
+    private ArrayAdapter<String> aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button3);
+        listContacts = new ArrayList<String>() ;
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, listContacts);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
