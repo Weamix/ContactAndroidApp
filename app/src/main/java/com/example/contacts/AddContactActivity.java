b2d0bd1bf2f373e61c15e7ae9561ae06f4ef5393
Weamix <maxime.vitse@odaxiom.com>
2020-05-09 20:16:20 +0200

    delete useless filldata on AddContactActivity

--- app/src/main/java/com/example/contacts/AddContactActivity.java
+++ app/src/main/java/com/example/contacts/AddContactActivity.java
@@ -25,22 +25,6 @@ public class AddContactActivity extends AppCompatActivity {
     private static ArrayList<String> listContacts;
     private ArrayAdapter<String> aa;
 
-    /* inutile dans AddContactAcvitiy car on n'a pas besoin d'afficher toute la liste des contacts?
-
-    private void fillData() {
-        // Get all of the contacts from the database and create the item list
-        Cursor c = db.fetchAllContacts();
-        startManagingCursor(c);
-
-        String[] from = new String[] { ContactsDbAdapter.KEY_NAME };
-        int[] to = new int[] { R.id.name,R.id.firstname };
-
-        // Now create an array adapter and set it to display using our row
-        SimpleCursorAdapter contacts =
-                new SimpleCursorAdapter(this, R.layout.activity_list_contacts, c, from, to);
-        list.setAdapter(contacts);
-    }*/
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +45,6 @@ public class AddContactActivity extends AppCompatActivity {
 
         db = new ContactsDbAdapter(this);
         db.open();
-        //fillData();
 
         add_contact.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,7 +57,6 @@ public class AddContactActivity extends AppCompatActivity {
                 phone.setText("");
                 email.setText("");
                 address.setText("");
-                //fillData();
             }
         });
 
