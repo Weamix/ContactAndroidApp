3f90c38186b0ef20dea73d6e257c5f4294e13c3e
WassimDjamaa <wassim.djamaa@sfr.fr>
2020-05-31 18:31:20 +0200

    add alert if fields null

--- app/src/main/java/com/example/contacts/AddContactActivity.java
+++ app/src/main/java/com/example/contacts/AddContactActivity.java
@@ -1,5 +1,6 @@
 package com.example.contacts;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -52,6 +54,16 @@ public class AddContactActivity extends AppCompatActivity {
                     add_contact.setEnabled(false); // bloque le bouton "Enregistrer"
                     //AlertDialog.Builder builder = new AlertDialog.Builder(this.);
                     //builder.setMessage(R.string.dialog_message).setTitle(R.string.dialog_title);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(AddContactActivity.this);
+                    builder.setMessage(R.string.dialog_infos).setTitle(R.string.infos);
+
+                    builder.setNegativeButton(R.string.close, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                        }
+                    });
+                    // Create the AlertDialog
+                    AlertDialog dialog = builder.create();
+                    dialog = builder.show();
                 }else {
                     listContacts.add(0, name.getText().toString());
                     listContacts.add(1,firstname.getText().toString());
