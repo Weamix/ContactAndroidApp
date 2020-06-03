ba8a0f4965caa76a750277e364cac4890884778a
Weamix <maxime.vitse@odaxiom.com>
2020-06-03 19:17:55 +0200

    qr code encoder

--- app/src/main/java/com/example/contacts/QrCodeActivity.java
+++ app/src/main/java/com/example/contacts/QrCodeActivity.java
@@ -10,8 +10,8 @@ public class QrCodeActivity {
 
     Bitmap bitmap ;
     public final static int QRcodeWidth = 400;
-    private static final int QRCodeWhiteColor = 0xFFFFFFFF;
-    private static final int QRCodeBlackColor = 0xFF000000;
+    private static final int blue = 0xFFFFFFFF;
+    private static final int white = 0xFF000000;
 
     /**
      * Source : https://www.it-swarm.dev/fr/android/android-comment-lire-le-code-qr-dans-mon-application/940979426/
@@ -41,8 +41,7 @@ public class QrCodeActivity {
 
             for (int x = 0; x < bitMatrixWidth; x++) {
 
-                pixels[offset + x] = bitMatrix.get(x, y) ?
-                        getResources().getColor(R.color.QRCodeBlackColor):getResources().getColor(R.color.QRCodeWhiteColor);
+                pixels[offset + x] = bitMatrix.get(x, y) ? blue:white;
             }
         }
         Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
